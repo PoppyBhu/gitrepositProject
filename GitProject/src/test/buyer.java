1bbe543224f37dd726371d397838a6634cd5e086
Anu <Anu@LAPTOP-618CED43.attlocal.net>
2022-01-17 15:24:50 -0500

    Commit review

---
+++ GitProject/src/test/buyer.java
@@ -0,0 +1,10 @@
+package test;
+
+public class buyer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ GitProject/src/test/seller.java
@@ -0,0 +1,10 @@
+package test;
+
+public class seller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
